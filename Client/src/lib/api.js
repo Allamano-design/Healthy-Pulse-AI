2cd1858bd3e6917a5f35acb7493914ca6aef61f6
Allamano Kinyahwe <allamanojoseph00@gmail.com>
2026-04-27 15:15:36 +0300

    fix: define api variable before exporting

--- Client/src/lib/api.js
+++ Client/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://healthy-pulse-ai.onrender.com";
+const API_URL = "https://healthy-pulse-ai-server.onrender.com";
 
 export const signup = async (userData) => {
   return await axios.post(`${API_URL}/api/auth/signup`, userData);
